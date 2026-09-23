a275e76643217047efeaf29fc4bd52d0eabf67e5
ak705578854-sys <ak705578854@gmail.com>
2026-09-23 16:56:55 +0530

    Replace test div with App component in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <div style={{ padding: "50px", fontSize: "30px" }}>
-    EMMC TEST
-  </div>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
